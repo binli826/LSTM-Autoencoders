d667c9ea05ecc5ba2825f3fdb729122be9a48d16
longqikaka <longqikaka@gmail.com>
2018-01-26 18:17:11 +0100

    ,,

--- KafkaConsumer.py
+++ KafkaConsumer.py
@@ -11,20 +11,50 @@ import tensorflow as tf
 from EncDecAD import EncDecAD
 modelpath = "C:/Users/Bin/Desktop/Thesis/tmp/LSTMAutoencoder_kdd99_v1.ckpt.meta"
 ckpt_dir = "C:/Users/Bin/Desktop/Thesis/tmp/LSTMAutoencoder_kdd99_v1.ckpt"
+paras = pd.Series(pd.read_csv(ckpt_dir + "parameters.csv",header=None)[0])
 
+batch_num  = paras.iloc[0]
+hidden_num = paras.iloc[1]
+step_num = paras.iloc[2]
+elem_num = paras.iloc[3]
+threshold  = paras.iloc[4]
+mu = 
+sigma = 
 batch_num = 20
 hidden_num = 100
 step_num = 20
-
-
-
+elem_num = sn.shape[1]
+threshold = 0.6
+#
+#p_input = tf.placeholder(tf.float32, shape=(batch_num, step_num, elem_num))
+#p_inputs = [tf.squeeze(t, [1]) for t in tf.split(p_input, step_num, 1)]
+#
+#ae = EncDecAD(hidden_num, p_inputs, decode_without_input=False)
 
 with tf.Session() as sess:
     saver = tf.train.import_meta_graph(modelpath)
     saver.restore(sess, ckpt_dir)
     print("Model restored.") 
     print('Initialized')
+
+        
     graph = tf.get_default_graph()
+    init = tf.global_variables_initializer()
+    
+    batch_num = graph.get_tensor_by_name("v_batch_num:0")
+    hidden_num = graph.get_tensor_by_name("v_hidden_num:0")
+    step_num = graph.get_tensor_by_name("v_step_num:0")
+    elem_num = graph.get_tensor_by_name("v_elem_num:0")
+    #threshold = graph.get_tensor_by_name("v_threshold:0")
+#    mu = graph.get_tensor_by_name("v_mu:0")
+#    sigma = graph.get_tensor_by_name("v_aigma:0")
+    
+    sess.run(init)
+    batch_num,hidden_num,step_num,elem_num = sess.run([batch_num,hidden_num,step_num,elem_num])
+   
+    
+    
+    
     normal_score = []
     n_in = []
     n_out = []
@@ -64,7 +94,7 @@ with tf.Session() as sess:
     bar = threshold*np.ones(len(normal_score)+len(abnormal_score))
     pd.Series(bar).plot(label="threshold")
     
-    
+def evaluation(abnormal_score,normal_score,threshold):
     beta = 0.5
     tp = np.array(abnormal_score)[np.array(abnormal_score)>threshold].size
     fp = len(abnormal_score)-tp
@@ -73,4 +103,4 @@ with tf.Session() as sess:
     P = tp/(tp+fp)
     R = tp/(tp+fn)
     fbeta= (1+beta*beta)*P*R/(beta*beta*P+R)
-    print(tp,fp,tn,fn,P,R)
+    print(fbeta,tp,fp,tn,fn,P,R)
